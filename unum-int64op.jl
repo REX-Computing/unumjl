133d9eebe9a04f5c1640dbf5d41da7351106f0c4
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2015-11-03 11:37:36 -0800

    modified:   ../unum-int64op.jl

--- unum-int64op.jl
+++ unum-int64op.jl
@@ -122,27 +122,8 @@ end
 #conversion to assembler more straightforward.  These are very accelerated
 #binary search modules, but even these should be changed in future revs.
 
-#a 16-element lookup array to speed up clz in the last few sections.
-              #0000   0001   0010   0011 0100 0101 0110 0111 1000 ...
-__clz_array=[0x0004,0x0003,0x0002,0x0002, o16, o16, o16, o16, z16,z16,z16,z16,z16,z16,z16,z16]
-function clz(n::UInt64)
-  (n == 0) && return 64
-  res::UInt16 = 0
-  #use the binary search method
-  (n & 0xFFFF_FFFF_0000_0000 == 0) && (n <<= 32; res += 0x0020)
-  (n & 0xFFFF_0000_0000_0000 == 0) && (n <<= 16; res += 0x0010)
-  (n & 0xFF00_0000_0000_0000 == 0) && (n <<= 8;  res += 0x0008)
-  (n & 0xF000_0000_0000_0000 == 0) && (n <<= 4;  res += 0x0004)
-  res + __clz_array[(n >> 60) + 1]
-end
 
-function clz(n::GI16)
-  res::UInt16 = 0
-  (n & 0xFF00 == 0) && (n <<= 8;  res += 0x0008)
-  (n & 0xF000 == 0) && (n <<= 4;  res += 0x0004)
-  res += __clz_array[(n >> 12) + 1]
-end
-#for when it's a superint (that's not a straight UInt64)
+clz(n::UInt64) = leading_zeros(n)
 
 function clz(n::Array{UInt64, 1})
   #iterate down the array starting from the most significant cell
@@ -155,29 +136,7 @@ function clz(n::Array{UInt64, 1})
   res
 end
 
-#a 16-element lookup array to speed up ctz in the last few sections.
-              #0000  0001 0010 0011   0100 0101 0110 0111    1000  1001 1010 1011   1100  1101 1110 1111
-__ctz_array=[0x0004, z16, o16, z16, 0x0002, z16, o16, z16, 0x0003, z16, o16, z16, 0x0002, z16, o16, z16]
-function ctz(n::UInt64)
-  (n == 0) && return 64
-  res::UInt16 = 0
-  (n & 0x0000_0000_FFFF_FFFF == 0) && (n >>= 32; res += 0x0020)
-  (n & 0x0000_0000_0000_FFFF == 0) && (n >>= 16; res += 0x0010)
-  (n & 0x0000_0000_0000_00FF == 0) && (n >>= 8;  res += 0x0008)
-  (n & 0x0000_0000_0000_000F == 0) && (n >>= 4;  res += 0x0004)
-  #unlike clz, ctz doesn't consume bits as it gets pushed around.  Let's mask
-  #out all digits we didn't shift over.
-  n &= 0x0000_0000_0000_000F
-  res + __ctz_array[n + 1]
-end
-
-function ctz(n::GI16)
-  res::UInt16 = 0
-  (n & 0x00FF == 0) && (n >>= 8;  res += 0x0008)
-  (n & 0x000F == 0) && (n >>= 4;  res += 0x0004)
-  n &= 0x000F
-  res + __ctz_array[n + 1]
-end
+ctz(n::UInt64) = trailing_zeros(n)
 
 #for when it's a superint (that's not a straight UInt64)
 function ctz(n::Array{UInt64, 1})
