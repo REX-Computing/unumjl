abfc7280d1e7c89694146d8e23c8239a92cefe9e
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2015-11-24 13:35:03 -0800

    rounded out the list of unum properties
    	modified:   src/unum/unum-properties.jl

--- src/unum/unum-properties.jl
+++ src/unum/unum-properties.jl
@@ -11,22 +11,21 @@ decode_exp(x::Unum) = decode_exp(x.esize, x.exponent)
 export decode_exp
 
 #some really dumb ones, but we'll put these in for legibility.
-is_ulp(x::Unum) = ((x.flags & UNUM_UBIT_MASK) != 0)
-is_exact(x::Unum) = ((x.flags & UNUM_UBIT_MASK) == 0)
-is_negative(x::Unum) = ((x.flags & UNUM_SIGN_MASK) != 0)
-is_positive(x::Unum) = ((x.flags & UNUM_SIGN_MASK) == 0)
-is_neg_def(x::Unum) = (!is_zero(x)) && is_negative(x)
-is_pos_def(x::Unum) = (!is_zero(x)) && is_positive(x)
+is_ulp{ESS,FSS}(x::Unum{ESS,FSS})      = ((x.flags & UNUM_UBIT_MASK) != 0)
+is_exact{ESS,FSS}(x::Unum{ESS,FSS})    = ((x.flags & UNUM_UBIT_MASK) == 0)
+is_negative{ESS,FSS}(x::Unum{ESS,FSS}) = ((x.flags & UNUM_SIGN_MASK) != 0)
+is_positive{ESS,FSS}(x::Unum{ESS,FSS}) = ((x.flags & UNUM_SIGN_MASK) == 0)
+is_neg_def{ESS,FSS}(x::Unum{ESS,FSS})  = (!is_zero(x)) && is_negative(x)
+is_pos_def{ESS,FSS}(x::Unum{ESS,FSS})  = (!is_zero(x)) && is_positive(x)
 export is_ulp, is_exact, is_negative, is_positive
 
 #a couple of testing conditions
-@gen_code function Base.isnan{ESS,FSS}(x::Unum{ESS,FSS})
+@gen_code function __is_nan_or_inf{ESS,FSS}(x::Unum{ESS,FSS})
   #calculate expected fsize and esize.
-  xfsize = (1 << FSS - 1)
-  xesize = (1 << ESS - 1)
-  xexponent = 1 << (1 << ESS) - 1
+  xesize = max_esize(ESS)
+  xfsize = max_fsize(FSS)
+  xexponent = max_exponent(ESS)
   @code quote
-    is_ulp(x) || return false
     (x.fsize == $xfsize) || return false
     (x.esize == $xesize) || return false
     (x.exponent == $xexponent) || return false
@@ -34,69 +33,77 @@ export is_ulp, is_exact, is_negative, is_positive
 
   if FSS < 7
     xfraction == top_mask(xfsize)
-    @code :(return x.fraction == xfraction)
+    @code :(x.fraction == xfraction)
   else
-    @code :(return is_all_ones(x.fraction))
+    @code :(is_all_ones(x.fraction))
   end
 end
-is_nan(x) = isnan(x)                                                            #alias the unum form with the julia.
+
+is_nan(x::Unum{ESS,FSS}) = is_ulp(x) && __is_nan_or_inf(x)
+Base.isnan(x::Unum{ESS,FSS}) = is_ulp(x) && __is_nan_or_inf(x)
 export is_nan
 
-import Base.isinf
-import Base.isfinite
 #isinf matches the julia definiton and triggers on either positive or negative
 #infinity.  is_pos_inf and is_neg_inf both are Unum-specific functions that detect
 #the expected values.
-is_inf{ESS,FSS}(x::Unum{ESS,FSS}) = is_exact(x) && (x.esize == 1 << ESS - 1) && (x.exponent == mask(1 << ESS)) && (x.fsize == 1 << FSS - 1) && (x.fraction == fillbits(-(1 << FSS), UInt16(length(x.fraction))))
-is_pos_inf{ESS, FSS}(x::Unum{ESS, FSS}) = is_positive(x) && is_inf(x)
-is_neg_inf{ESS, FSS}(x::Unum{ESS, FSS}) = is_negative(x) && is_inf(x)
-#caution, isfinite is a rather slow algorithm
-is_finite{ESS,FSS}(x::Unum{ESS,FSS}) = (x.exponent != mask(1 << ESS)) || (x.fraction != fillbits(-1 << FSS, UInt16(length(x.fraction))))
+is_inf{ESS,FSS}(x::Unum{ESS,FSS}) = is_exact(x) && __is_nan_or_inf(x)
+is_pos_inf{ESS, FSS}(x::Unum{ESS, FSS}) = is_positive(x) && is_exact(x) && __is_nan_or_inf(x)
+is_neg_inf{ESS, FSS}(x::Unum{ESS, FSS}) = is_negative(x) && is_exact(x) && __is_nan_or_inf(x)
+export is_inf, is_pos_inf, is_neg_inf
 
-#aliasing and exporting
-isinf{ESS,FSS}(x::Unum{ESS,FSS}) = is_inf(x)                                                            #alias the unum form with the julia form
-isfinite{ESS,FSS}(x::Unum{ESS,FSS}) = is_finite(x)                                                      #alias the unum form with the julia form
-export isinf, is_inf, is_pos_inf, is_neg_inf, isfinite, is_finite
+#aliasing
+Base.isinf{ESS,FSS}(x::Unum{ESS,FSS}) = is_exact(x) && __is_nan_or_inf(x)
 
-import Base.issubnormal
+@gen_code function is_finite{ESS,FSS}(x::Unum{ESS,FSS})
+  #record the maximum esize and fsize values.  Any value less than this and
+  #it's finite.
+  mesize = max_esize(ESS)
+  mfsize = max_fsize(FSS)
+  @code quote
+    x.esize < $mesize && return true
+    x.fsize < $mfsize && return true
+  end
+  if (FSS < 7)
+    mfraction = top_mask(mfsize)
+    @code :(x.fraction < $mfraction)
+  else
+    @code :(!is_all_ones(x.fraction))
+  end
+end
+Base.isfinite{ESS,FSS}(x::Unum{ESS,FSS}) = is_finite(x)
+
 #NB:  The difference between "is_exp_zero" and "issubnormal" - is_exp_zero admits
 #zero as a solution; issubnormal is in compliance with the standard julia
 #issubnormal function and does not admit zero as a true result.
-is_subnormal{ESS,FSS}(x::Unum{ESS,FSS}) = (x.exponent == z64) && ((ESS > 6) ? !(allzeros(x.fraction)) : x.fraction != 0)
+is_subnormal{ESS,FSS}(x::Unum{ESS,FSS}) = (x.exponent == z64) && is_not_zero(x.fraction)
 is_exp_zero{ESS,FSS}(x::Unum{ESS,FSS}) = x.exponent == z64
-is_strange_subnormal{ESS,FSS}(x::Unum{ESS,FSS}) = is_exp_zero(x) && (x.esize < max_esize(ESS))
+@gen_code function is_strange_subnormal{ESS,FSS}(x::Unum{ESS,FSS})
+  mesize = max_esize(ESS)
+  @code :((x.exponent == z64) && (x.esize < $mesize))
+end
 
-issubnormal{ESS,FSS}(x::Unum{ESS,FSS}) = is_subnormal(x)                       #alias the unum-form to the julia-compliant form.
+Base.issubnormal{ESS,FSS}(x::Unum{ESS,FSS}) = is_subnormal(x)                    #alias the unum-form to the julia-compliant form.
 #use ESS because this will be checked by the compiler, instead of at runtime.
-is_frac_zero{ESS,FSS}(x::Unum{ESS,FSS}) = (ESS > 6) ? allzeros(x.fraction) : (x.fraction == 0)
+is_frac_zero{ESS,FSS}(x::Unum{ESS,FSS}) = is_all_zero(x.fraction)
 
 is_zero(x::Unum) = (x.exponent == z64) && is_exact(x) && is_frac_zero(x)
 
-#helper function that checks if a superint is of the form 10000...0000
-function __top_frac{ESS,FSS}(x::Unum{ESS,FSS})
-  last(x.fraction) != t64 && return false
-  for idx = 2:length(x.fraction)
-    x.fraction[idx] != z64 && return false
-  end
-  return true
-end
 
-#annoyingly, asymmetric exponents make the process of checking unit values more labourious.
 function is_unit{ESS,FSS}(x::Unum{ESS,FSS})
+  #asymmetric exponents make this slightly more laborious than might be expected
   #one is not an ulp, it is exact.
   is_ulp(x) && return false
   #case one:  An exponent of zero and nothing in the fraction.
   #note that when x.esize == 0, then decode_exp 0 is subnormal.
-  (x.esize != 0) && (decode_exp(x) == 0) && is_frac_zero(x) && return true
-  #case two:  A short exponent and
-  is_subnormal(x) && (x.esize == 0) && (__top_frac(x)) && return true
+  (x.esize != 0) && (decode_exp(x) == 0) && is_all_zero(x.fraction) && return true
+  is_subnormal(x) && (x.esize == 0) && (is_top(x.fraction)) && return true
   return false
 end
 
 is_one(x::Unum) = is_positive(x) && is_unit(x)
 is_neg_one(x::Unum) = is_negative(x) && is_unit(x)
 #checks if the value is sss ("smaller than small subnormal")
-is_sss(x::Unum) = (x.exponent == z64) && is_ulp(x) && is_frac_zero(x)
+is_sss(x::Unum) = (x.exponent == z64) && is_ulp(x) && is_all_zero(x.fraction)
 is_pos_sss(x::Unum) = is_positive(x) && is_sss(x)
 is_neg_sss(x::Unum) = is_negative(x) && is_sss(x)
 #checks if the value is more than maxreal
