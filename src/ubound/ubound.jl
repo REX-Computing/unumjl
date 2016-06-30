18e3c5cd5c373c714348ad7f8808e4805033e116
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2016-06-29 20:22:21 -0700

    restructured ubound directory to have maths in its own subsection
    	renamed:    src/ubound/ubound-addition.jl -> src/ubound/math/ubound-addition.jl
    	renamed:    src/ubound/ubound-comparison.jl -> src/ubound/math/ubound-comparison.jl
    	renamed:    src/ubound/ubound-division.jl -> src/ubound/math/ubound-division.jl
    	renamed:    src/ubound/ubound-multiplication.jl -> src/ubound/math/ubound-multiplication.jl
    	renamed:    src/ubound/ubound-sqrs.jl -> src/ubound/math/ubound-sqrs.jl
    	renamed:    src/ubound/ubound-subtraction.jl -> src/ubound/math/ubound-subtraction.jl
    	modified:   src/ubound/ubound.jl

--- src/ubound/ubound.jl
+++ src/ubound/ubound.jl
@@ -2,6 +2,6 @@
 #collation includes for all the code that supports the ubound type (but not the
 #type definition itself, that must be forwarded separately.)
 
-include("ubound-comparison.jl")
+include("./math/ubound-comparison.jl")
 include("ubound-resolve.jl")
 include("ubound-hlayer.jl")
